490fe0dd3c78dab0e5d6fa20207faaa1e37c3dd5
Joao Marcelo <joaomarcelo.as12@gmail.com>
2022-08-17 22:53:28 -0300

    Entrega checkpoint

---
+++ checkpoint.js
@@ -0,0 +1,48 @@
+var item = "Macarrão";
+var tempo = 20;
+if(item === "Pipoca" && tempo > 10*3){
+    console.log("kabuuum")
+}else if(item === "Pipoca" && tempo < 10){
+    console.log("tempo insuficiente")
+}else if(item === "Pipoca" && tempo > 10*2){
+    console.log("comida queimou")
+}else if(item === "Pipoca" && tempo === 10 ){
+    console.log("sucesso")
+}else if(item === "Macarrão" && tempo > 8*3){
+    console.log("kabuuum")
+}else if(item === "Macarrão" && tempo < 8){
+    console.log("tempo insuficiente")
+}else if(item === "Macarrão" && tempo > 8*2){
+    console.log("comida queimou")
+}else if(item === "Macarrão" && tempo === 8 ){
+    console.log("sucesso")
+}else if(item === "Carne" && tempo > 15*3){
+    console.log("kabuuum")
+}else if(item === "Carne" && tempo < 15){
+    console.log("tempo insuficiente")
+}else if(item === "Carne" && tempo > 15*2){
+    console.log("comida queimou")
+}else if(item === "Carne" && tempo === 15 ){
+    console.log("sucesso")
+}else if(item === "Feijão" && tempo > 36){
+    console.log("kabuuum")
+}else if(item === "Feijão" && tempo < 12){
+    console.log("tempo insuficiente")
+}else if(item === "Feijão" && tempo > 24){
+    console.log("comida queimou")
+}else if(item === "Feijão" && tempo === 12 ){
+    console.log("sucesso")
+}else if(item === "Brigadeiro" && tempo > 24){
+    console.log("kabuuum")
+}else if(item === "Brigadeiro" && tempo < 8){
+    console.log("tempo insuficiente")
+}else if(item === "Brigadeiro" && tempo > 16){
+    console.log("comida queimou")
+}else if(item === "Brigadeiro" && tempo === 8 ){
+    console.log("sucesso")
+}else{
+    console.log("prato inexistente")
+}
+
+console.log("prato pronto, bom apetite")
+// Tentei fazer com switch mas não consegui //
